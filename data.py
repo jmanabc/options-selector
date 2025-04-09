54705c6c8a642dbc913aedef52dc14ad5c2d820f
jmanabc <jmanabc1@gmail.com>
2025-04-09 15:56:12 -0400

    fix

--- data.py
+++ data.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 def get_options_data(ticker):
-    df = pd.read_csv('strikes_202402091600.csv')
+    df = pd.read_csv('strikes_aapl.csv')
     df_ticker = df[df['ticker'] == f'{ticker}']
     df_ticker.loc[:, 'tradeDate'] = pd.to_datetime(df_ticker['tradeDate'])
 
